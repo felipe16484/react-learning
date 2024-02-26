c67d4a6208d894eb0180a97cc7803002b8fca88f
desarrollador4@bscloud.com <felipecg_018@hotmail.com>
2024-02-26 08:31:47 -0500

    Prueba otro user

--- curso-midudev/jsx_usage/00-hola-mundo/src/TwitterFollowCard.jsx
+++ curso-midudev/jsx_usage/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react" 
 export function TwitterFollowCard ({ children, userName }) {
-
+    // Uso del "useState"
     const [isFollowing,setIsFollowing] = useState(false)    
     
     const text = isFollowing ? 'Siguiendo' : 'Seguir'
